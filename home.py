83d9200f337b3d3e29843ec2b2058c2698ee463d
Bolatbayev <bolatbayev08@gmail.com>
2026-01-30 00:16:11 +0500

    add casting.py file, add comments.py file, add data_types.py file, add home.py file, add numbers.py file, add string.py file, add syntax.py file, add variables.py file

---
+++ home.py
@@ -0,0 +1,12 @@
+#1 example
+print("Hello, World!")
+#2 example
+print("Hello","Python!")
+#3 example
+print(5+3)
+#4 example
+x = 20
+y = 4
+print(x + y)
+#5 example
+print("Bereket, krasavchik:)")
---
+++ variables.py
@@ -0,0 +1,32 @@
+#1 example variable name
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+#2 example assign multiple values
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+#3 example output variable
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+#4 example Global Variable
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+#5 example 
+x = 5
+y = "John"
+print(x)
+print(y) 
