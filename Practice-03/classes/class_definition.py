d444811b9b40f452837d2f362ba6c93fae6ce627
Bolatbayev <bolatbayev08@gmail.com>
2026-02-27 02:16:01 +0500

    f

---
+++ Practice-03/classes/class_definition.py
@@ -0,0 +1,15 @@
+# 1
+class Animal:
+    pass
+
+# 2
+class Car:
+    wheels = 4
+
+# 3
+class Student:
+    school = "ABC School"
+
+# 4
+class Book:
+    author = "Unknown"
---
+++ Practice-03/classes/class_methods.py
@@ -0,0 +1,19 @@
+# 1
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+# 2
+class Greeter:
+    def say_hello(self, name):
+        return f"Hello, {name}"
+
+# 3
+class Circle:
+    def area(self, r):
+        return 3.14 * r * r
+
+# 4
+class Counter:
+    def increment(self, value):
+        return value + 1
